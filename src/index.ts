d066ad20dd8c9dc4ece7389ad20e020b9b397d4a
Maks-eco <maks.zawulon@gmail.com>
2023-07-12 15:48:02 +0400

    finalize list style, add click listener

--- src/index.ts
+++ src/index.ts
@@ -24,3 +24,19 @@ const message: string | number = 3; //"Ts rulz";
 console.log(message.toString());
 
 // document.getElementsByClassName("container")[0].appendChild(component());
+
+const containerActivation = (state: boolean) => {
+  return "container container-" + (state ? "enactive" : "active");
+};
+
+const list = document.getElementsByClassName("container");
+for (let item = 0; item < list.length; item++) {
+  let element = list[item] as HTMLElement;
+  const input: HTMLInputElement = element.getElementsByTagName("input")[0];
+  // console.log(input.checked);
+  input.addEventListener("click", function () {
+    // console.log(this);
+    element.className = containerActivation(input.checked);
+  });
+  element.className = containerActivation(input.checked);
+}
