9dbc6da036660a2fc1cbbb975e49401a88d7e033
Maks-eco <maks.zawulon@gmail.com>
2023-09-25 21:56:31 +0400

    add toggling backgrd widgets on 600px resize

--- src/widgets/BackgroundMosaic/BackgroundMosaic.tsx
+++ src/widgets/BackgroundMosaic/BackgroundMosaic.tsx
@@ -6,6 +6,7 @@ import cls from "./BackgroundMosaic.module.scss";
 
 import { useState, useEffect, memo } from "react";
 import { TaskList } from "app/interfaces/StoreTest";
+import { useSelector } from "react-redux";
 
 interface ImgInfo {
   id: number;
@@ -16,24 +17,13 @@ interface ImgInfo {
 const storage = new ListStorage<Task>();
 
 function BackgroundMosaic() {
-  // const tasks = useSelector((state: TaskList) => state.list);
-  // const items = [1, 2, 3, 4, 5, 6];
+  const windowWidth = useSelector((state: TaskList) => state.windowWidth);
 
   const allImages: ImgInfo[] = Images.map((img, index) => {
     return { id: index, index, state: true };
   });
 
-  const [items, setImgNumb] = useState<ImgInfo[]>(
-    /* [
-    { index: 1, state: true },
-    { index: 2, state: true },
-    { index: 3, state: true },
-    { index: 4, state: true },
-    { index: 5, state: true },
-    { index: 6, state: true },
-  ] */
-    allImages.slice(0, 6)
-  );
+  const [items, setImgNumb] = useState<ImgInfo[]>(allImages.slice(0, 6));
 
   const [queue, setImgQueue] = useState<ImgInfo[]>(allImages.slice(6));
 
@@ -41,31 +31,22 @@ function BackgroundMosaic() {
 
   useEffect(() => {
     const timer = setInterval(() => {
-      // const first = items.shift();
-      // setImgNumb([...items, first]);
       bufQueueIter = [...queue];
 
       const some = items.map((it, index) => {
-        // it.index = index;
         const oldState = it.state;
         const state = Math.random() > 0.3 ? true : false;
         if (oldState === true && state === false) {
           const bufIt = { ...it };
           it.index = bufQueueIter.shift().index;
-          // setImgQueue([...queue.slice(1), bufIt]);
-
-          // console.log(bufQueueIter);
           bufQueueIter.push(bufIt);
-          // console.log(bufQueueIter);
         }
         return { id: it.id, index: it.index, state };
       });
       setImgQueue(bufQueueIter);
-      // console.log(queue);
-      // console.log(some);
       setImgNumb(some);
     }, 2000);
-    // console.log(myRef.current.className);
+
     return () => {
       clearTimeout(timer);
     };
@@ -81,7 +62,6 @@ function BackgroundMosaic() {
           item.state ? cls["temporary-hidden"] : ""
         }`}
       >
-        {/* {item} */}
         <div
           className={cls.anistep}
           dangerouslySetInnerHTML={{ __html: Images[item.index].toString() }}
@@ -90,9 +70,9 @@ function BackgroundMosaic() {
     </div>
   ));
 
-  return <div className={cls.container}>{listItems}</div>;
+  return windowWidth <= 600 ? (
+    <div className={cls.container}>{listItems}</div>
+  ) : null;
 }
 
 export default BackgroundMosaic;
-// const BackgroundMosaic = memo(unBackgroundMosaic);
-// export default BackgroundMosaic;
